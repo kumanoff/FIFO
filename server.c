7cba1221bd00dde0a9a67b41595d9903c793c99c
kumanoff <mayeriegor@yandex.ru>
2023-01-27 23:58:32 +0700

    modified:   server.c modified:   client.c

--- server.c
+++ server.c
@@ -10,7 +10,7 @@
 #include <fcntl.h>
 
 int main(int argc, char* argv[]){
-    int i, pid[argc], fd;
+    int i, pid[argc], input_Fd;
     char arg[40];
     if (argc<2) {
         printf("Usage: file textfile1...\n");
@@ -19,24 +19,23 @@ int main(int argc, char* argv[]){
     strcpy(arg,argv[i]);
 
 
-    if (mkfifo("./myfifo_1", 0700) == -1){//this file can be used by everybody if 0777
+    if (mkfifo("myfifo_1", 0700) == -1){//this file can be used by everybody if 0777
         printf("unable to create fifo file \n error");
         return 1;
     }
-
-
-    pid[i] = fork(); 
+    /*pid[i] = fork(); 
     if (pid[i] == 0) {      // child process
-        fd = open("myfifo_1", O_WRONLY);
-        if (fd == -1){
+        
+    }*/
+    input_Fd = open("myfifo_1", O_WRONLY);
+        if (input_Fd == -1){
             printf("can't open \n");
             return 1;
         }   
-        if( write(fd, argv[1], sizeof(int))){
+        if( write(input_Fd, argv[1], sizeof(int)) == -1){
             printf("cant' write \n");
             return 3;
         }
-        close(fd);
-    }
+        close(input_Fd);
     return 0;
 }
