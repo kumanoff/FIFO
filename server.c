13e4cc475aaa8626817652247892f66b13ba4d29
kumanoff <mayeriegor@yandex.ru>
2023-01-31 21:13:22 +0700

    modified:   a.out 	new file:   out.txt 	modified:   server.c 	modified:   server.exe

--- server.c
+++ server.c
@@ -5,11 +5,21 @@
 #include<sys/stat.h>
 #include<string.h>
 #include <errno.h>
+#include <locale.h>
+
+
 int main() {
+   setlocale(LC_ALL, "Rus");
    char fname[25] = "";
-   char fcontent[2000] = "";
-   char file_not_exist = "file do not exist\n";
-   int fd, fd1, fd2;
+   char fcontent;
+   char file_not_exist[] = "NO WAY\n";
+   char SUCCESS[] = "SUCCESS\n";
+   int fd, fd1, fd2, fd3;
+   int numRead, total, openFlags;
+   mode_t filePerms;
+   openFlags = O_CREAT | O_WRONLY | O_TRUNC;
+   filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
+
    if ((mkfifo("fifo1", 0600) < 0) && (errno != EEXIST)){
       printf("can't create fifo1\n");
       exit(-1);
@@ -32,19 +42,36 @@ int main() {
       printf("error while reading fifo file  \n");
       exit(-6);
    }
-   fd2 = open(fname, O_RDONLY);//file that we are open up
+   fd2 = open(fname, O_RDONLY);//text file that we are open 
    if (fd2 == -1) {
-        printf("Error opening fifo file\n");
+        printf("Error opening text file file\n");
         exit(-5);
-    }  
-   //printf("On server side: \n");
-   while (read(fd2, fcontent, sizeof(int)) != 0) {
-      //printf("%s", fcontent);
-      if (fd < 0)
+    } 
+    fd3 = open("out.txt", openFlags, filePerms);//text file that we are write 
+   if (fd3 == -1) {
+        printf("Error opening new text file\n");
+        exit(-5);
+    }
+
+   total = 0;
+   while ((numRead = read(fd2, &fcontent, 1)) > 0) {
+      if (fd < 0){
          write(fd1, file_not_exist, strlen(file_not_exist));//send message "file do not exist"
-      else
-         write(fd1, fcontent, strlen(fcontent));
+         exit(-6);
+      }
+      else{
+         
+         write(fd3, &fcontent, 1);
+         printf("fcontent = %s \n", &fcontent);
+         if (fcontent == '.') {
+            fcontent = 0x0a;
+            write(fd3, &fcontent, numRead);
+            total++;
+         }
+      }
    }
+   write(fd1, SUCCESS, strlen(SUCCESS));//send to client 
+   printf("Amount of operations: %d\n", total);
    close (fd);
    close (fd1); 
    close (fd2); 
